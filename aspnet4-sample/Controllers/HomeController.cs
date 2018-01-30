5c1fbf53b62b276218ff363351b7f767d8d18b1f
akilagithub <30523620+akilagithub@users.noreply.github.com>
2018-01-30 15:14:15 +0530

    Edited by Akila

--- aspnet4-sample/Controllers/HomeController.cs
+++ aspnet4-sample/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace aspnet4_sample.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page here.";
 
             return View();
         }
